35a8279a90160c321717b3d83634f79d6697711c
Huy.Tran <huy.tran@smartaction.com>
2023-12-11 19:30:02 +0700

    APPS-01 | Initialize

---
+++ src/App.js
@@ -0,0 +1,61 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 FAR AWAY 🌴</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        👜 You hav X items in your package list, and you already packaged X (X%)
+      </em>
+    </footer>
+  );
+}
